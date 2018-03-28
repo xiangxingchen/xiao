82ecaa15aaa61165c80b82ae759e73aff2d2a6f0
陈祥 <1447388624@qq.com>
2018-03-28 22:16:47 +0800

    学习登录

--- client/app.js
+++ client/app.js
@@ -4,7 +4,68 @@ var config = require('./config')
 
 App({
     onLaunch: function (options) {
-      console.log(this, options);
-        qcloud.setLoginUrl(config.service.loginUrl)
+      qcloud.setLoginUrl(config.service.loginUrl);
+      // 展示本地存储能力
+      var logs = wx.getStorageSync('logs') || []
+      logs.unshift(Date.now())
+      wx.setStorageSync('logs', logs)
+
+      // 登录
+      wx.login({
+        success: res => {
+          console.log('1111111', res);
+          // 发送 res.code 到后台换取 openId, sessionKey, unionId
+          if (res.code) {
+            //发起网络请求
+            wx.request({
+              url: config.service.loginUrl,
+              data: {
+                code: res.code
+              },
+              success: res => {
+                console.log('4321',res);
+              }
+            })
+          } else {
+            console.log('登录失败！' + res.errMsg)
+          }
+        }
+      })
+      // 获取用户信息
+      wx.getSetting({
+        success: res => {
+          console.log('123', res)
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+            wx.getUserInfo({
+              success: res => {
+                // 可以将 res 发送给后台解码出 unionId
+                this.globalData.userInfo = res.userInfo
+
+                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                // 所以此处加入 callback 以防止这种情况
+                if (this.userInfoReadyCallback) {
+                  this.userInfoReadyCallback(res)
+                }
+              }
+            })
+          }
+          if (!res.authSetting['scope.record']) {
+            wx.authorize({
+              scope: 'scope.record',
+              success() {
+                // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+                wx.startRecord()
+              }
+            })
+          }
+        },
+        fail: res => {
+          console.log(res);
+        }
+      })
+    },
+    globalData: {
+      userInfo: null
     }
 })
